4724479d254844c759299665d508c84146e846a9
Matthias Glenk <glenkmatthias@gmail.com>
2024-02-05 19:14:47 +0100

    first sidebar

---
+++ script.js
@@ -0,0 +1,9 @@
+const sidebar = document.querySelector('.sidebar');
+
+function showSidebar() {
+    sidebar.style.display = 'flex';
+}
+
+function showSidebar2() {
+    sidebar.style.display = 'flex';
+}
