deb7f4a8ffc590810f36425da7b27bf859d4289f
Matthias Glenk <matthias.glenk@aracom.de>
2024-05-28 10:08:37 +0200

    remove banner

--- ff-villenbach/src/app/header/header.component.ts
+++ ff-villenbach/src/app/header/header.component.ts
@@ -20,13 +20,7 @@ export class HeaderComponent {
   menuOptions: string[] = ["Home", "Aktuelles", "Aktive Wehr", "Jugend"]
   mobileMenuOpen: boolean = false;
 
-  showBanner: boolean = true;
-
-  bannerMessage: string = "🔥150 Jahre Feuerwehr Villenbach - Festwochenende 5.7.2025 und 6.7.2025🔥"
-
   toggleMobileNavbar() {
-    let message = this.mobileMenuOpen ? "menu is now closed" : "menu is now open"
-    console.log(message)
     this.mobileMenuOpen = !this.mobileMenuOpen;
   }
 
