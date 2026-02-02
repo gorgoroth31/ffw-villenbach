89e1437f8c38b96b023730b46569ae32e758d2bd
gorgoroth31 <it.glen@mailbox.org>
2026-02-02 01:22:55 +0100

    added breadcrumbs but somehow dont work on dev

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,34 @@
+<template>
+  <span id="breadcrumbs-container">
+    <a v-for="(breadcrumb, index) of allBreadcrumbs()" :href="breadcrumb[1]">
+      {{ breadcrumb[0] }}<span v-if="index !== allBreadcrumbs().length - 1" class="breadcrumb-divider">/</span>
+    </a>
+  </span>
+</template>
+
+<script setup lang="ts">
+
+  const props = defineProps<{
+    breadcrumbs: [string, string][]
+  }>()
+
+  function allBreadcrumbs() {
+    if (props.breadcrumbs !== null && props.breadcrumbs !== undefined) {
+      return [["Startseite", "/"], ...props.breadcrumbs]
+    }
+    return [["Startseite", "/"]];
+  }
+</script>
+
+<style scoped lang="css">
+  a{
+      text-decoration: none;
+      color: gray;
+  }
+
+  .breadcrumb-divider {
+    color: gray;
+    margin: 0 .5vw;
+  }
+
+</style>
